d16b96bc49104051287e9dbbb72c298e6eb80ceb
moatz mohamed <moatz_3550@hotmail.com>
2019-12-08 13:23:28 +0200

    updated gitignore

--- commands/activity.js
+++ commands/activity.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 module.exports.run = (bot , msg , args) => {
 
+  JSON.parse(fs.readFileSync('./jsonfiles/activity.json' , 'utf8'));
+
 try {
   if (msg.author.id === '198426222810628106') {
 
@@ -36,7 +38,7 @@ try {
       msg.channel.send(`*Applying the new activity.* **"${newAct.activity}"**`)
       .then(change => {
         change.delete();
-        fs.writeFile('./jsonfiles/activity.json' , JSON.stringify(newAct) , (err) => {
+        fs.writeFileSync('./jsonfiles/activity.json' , JSON.stringify(newAct) , (err) => {
           if (err) console.log(err);
         });
         msg.channel.send(`**${newAct.activity}** is now applied`);
