bd2924af80751cf40c2b83d31d0049a37a510de0
Deva244 <49819715+Deva244@users.noreply.github.com>
2019-12-09 20:13:20 +0200

    Testing MongoDB

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Activity = require('./models/activity.js');
 const mongoose = require('mongoose');
 let DBusername = process.env.DBusername , DBpass = process.env.DBpass;
 console.log(DBusername , DBpass);
-let dburl = `mongodb+srv://${DBusername}:${DBpass}@frost-bot-db-cxybp.mongodb.net/Frost`;
+let dburl = 'mongodb+srv://' + DBusername + ':' + DBpass + '@frost-bot-db-cxybp.mongodb.net/Frost';
 mongoose.connect(dburl , {
   useNewUrlParser: true,
   useUnifiedTopology: true
